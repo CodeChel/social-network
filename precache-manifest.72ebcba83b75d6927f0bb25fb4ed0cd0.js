1599cefeb08fc39f780b2b91b3aa17420f81847b
ChelCod <vladislavbezverhnii@gmail.com>
2019-09-28 22:26:47 +0300

    Updates

---
+++ precache-manifest.72ebcba83b75d6927f0bb25fb4ed0cd0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc6762eb7008c52ae09e9d7ab23df0dd",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "bb4de2b0e5a9e6f57f15",
+    "url": "/social-network/static/css/3.c4a8e7ec.chunk.css"
+  },
+  {
+    "revision": "1b0d80b4e67a75e09936",
+    "url": "/social-network/static/css/main.d716dc7c.chunk.css"
+  },
+  {
+    "revision": "41414153633abde5f921",
+    "url": "/social-network/static/js/2.082296d9.chunk.js"
+  },
+  {
+    "revision": "bb4de2b0e5a9e6f57f15",
+    "url": "/social-network/static/js/3.2ea9a908.chunk.js"
+  },
+  {
+    "revision": "1b0d80b4e67a75e09936",
+    "url": "/social-network/static/js/main.91f249eb.chunk.js"
+  },
+  {
+    "revision": "69cf5d221423911c2fa3",
+    "url": "/social-network/static/js/runtime~main.40805d29.js"
+  },
+  {
+    "revision": "6fd59ae095bd0794d326713ac3aef0ea",
+    "url": "/social-network/static/media/Rolling-1s-200px.6fd59ae0.svg"
+  },
+  {
+    "revision": "03e01a87f57fab52a567b2f22fd9307f",
+    "url": "/social-network/static/media/ava.03e01a87.jpg"
+  }
+]);
